641d9ce6340a4b18a74b101abe67d6b3e43be969
StefanHB <stefan@hb-digit.com>
2020-04-24 10:44:30 +0200

    ajout dun loading screen avant darriver sur la page

---
+++ dist/script/transition.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $(".loader-wrapper").fadeOut("slow");
+        $(".myNavbar").css("visibility","visible");
+    }, 3000);
+ 
+});
