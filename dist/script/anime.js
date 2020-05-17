1af83d588b26aaa554bceea22eed6e7b17fa00c6
Stefan <32258096+StefanYYC@users.noreply.github.com>
2020-05-17 20:17:24 +0200

    delete anim polygon

--- dist/script/anime.js
+++ dist/script/anime.js
@@ -4,3 +4,18 @@ var myIcon = anime({
     delay: anime.stagger(100, {start: 2200}) // delay starts at 500ms then increase by 100ms for each elements.
   });
 
+var myPolymorph = anime({
+  targets: '.polymorph',
+  points: [
+    { value: [
+      '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
+      '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
+    },
+    { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+    { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+    { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+  ],
+  easing: 'easeOutQuad',
+  duration: 2000,
+  loop: false
+});
